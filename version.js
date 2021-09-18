2433dbac6f236818da9702f12416af834b547610
SuneetBansal <32817920+SuneetBansal@users.noreply.github.com>
2021-09-18 23:32:41 +0530

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,12 @@
-## Brahma Nightly
-BRAHMA_NIGHTLY=n12000
-## MELCHIOR Nightly
-MELCHIOR_NIGHTLY=m12340
+# Imp - if you are experimenting, change versions for _NIGHTLY only
+
+# Both PRD and DEV versions are saved in same server (bin.barco.com) now
+# Update version numbers for which artifacts needs to be pushed (not automatic purposely)
+
+# RELEASE versions are saved from master branch in PRD repos which is a permanent storage
+WEB_COMMUNICATOR_VERSION_RELEASE=1.6.1
+MELCHOIR_VERSION_RELEASE=1.0.1
+
+# NIGHTLY versions are saved from any other branches in DEV repos which is a temporary storage
+WEB_COMMUNICATOR_VERSION_NIGHTLY=n133
+MELCHOIR_VERSION_NIGHTLY=n128
